ed843f7a325d2e8aa5047525b8d8b935bf55ac04
trungnguyen <thangkhung156@gmail.com>
2021-06-06 21:18:05 +0700

    finish 13-12

--- event/src/App.js
+++ event/src/App.js
@@ -1,52 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
 
-function App() {
-  const [message, setMessage] = useState('Ready')
+import { useState } from 'react';
+import './App.css';
+import ThemeButton from './ThemeButton';
+
+export default function App () {
+
+  const [message, setMessage] = useState("Ready")
   const [counter, setCounter] = useState(0)
-  const [theme, setTheme] = useState('secondary')
+  const [theme, setTheme] = useState("secondary")
 
   const handleClick = (e, newTheme) => {
-    //e.persist()
-    setCounter(counter + 1)
-    console.log(counter)
-    setMessage('Event: ' + e.type + ':' + counter)
+    const newCounter = counter + 1
+    setCounter(newCounter)
     setTheme(newTheme)
+    setMessage("Event: " + newCounter)   
   }
 
-  const toogleCheckBox = (e) => {
-    if (counter === 0) {
-      e.preventDefault()
-    }
+  const selectTheme = (newTheme) => {
+    setTheme(newTheme)
+    setMessage("Theme: " + newTheme)
   }
-  
+
   return (
     <div className="m-2">
-
-      <div className = "form-check">
-        <input className="form-check-input" type="checkbox" onClick={toogleCheckBox}/>
-        <label>This is a checkbox</label>  
-      </div>
-      
-      <div className={"h4 bg-" + theme +" text-white text-center p-2"}>
+      <div className={"h4 bg-" + theme + " text-white text-center p-2"}>
         {message}
       </div>
-      <div className="text-center">
-        <button className="btn btn-primary"
-          onClick={(e) => handleClick(e, "primary")}
-        >
-          Normal
-        </button>
 
-        <button className="btn btn-primary"
-          onClick={(e) => handleClick(e, "danger")}
-        >
-          Danger
-        </button>
+      <div className="text-center">
+        <ThemeButton theme="primary" callback={selectTheme} />
+        <ThemeButton theme="danger" callback={selectTheme} />
       </div>
     </div>
-  );
-}
 
-export default App;
+  )
+   
+}
---
+++ lifecycle/src/List.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ActionButton from "./ActionButton"
+
+export default class List extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            names: ["Bob", "Alice", "Dora"]
+        }
+    }
+
+    reverseList = () => {
+        this.setState({names: this.state.names.reverse()})
+    }
+
+    render() {
+        console.log("Render list")
+        return(
+            <div>
+                <ActionButton callback={this.reverseList} text="Reverse names"/>
+                {
+                    this.state.names.map((name, index) => {
+                        return <h5 key={name}>{name}</h5>
+                    })
+                }
+            </div>
+        )
+    }
+
+}
