a367c937df9f7c93c523c8115f2a697e282b16aa
trungnguyen <thangkhung156@gmail.com>
2021-07-01 22:12:23 +0700

    finish 15-9

--- forms/src/components/Editor.js
+++ forms/src/components/Editor.js
@@ -3,15 +3,26 @@ import React, { Component, useState } from "react";
 export default class Editor extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            name: ""
-        }
-    }
 
+        this.state = {
+            name: "Bob",
+            flavor: "Vannilla",
+            toppings: ["Strawberries"]
+        }
+
+        this.flavors = ["Chocolate", "Double chocolate", "Triple chocolate", "Vanilla"]
+        this.toppings = ["Sprinkles", "Fudge sauce", "Strawberries", "Maple syrup"]
+    }
+    
     updateFormValue = (e) => {
         this.setState({[e.target.name]: e.target.value}, 
             () => this.props.submit(this.state)
         )
+    }
+
+    updateFormValueOptions = e => {
+        let options = [...e.target.options].filter(o => o.selected).map(o => o.value)
+        this.setState({[e.target.name]: options}, () => this.props.submit(this.state))
     }
 
     render() {
@@ -24,6 +35,33 @@ export default class Editor extends Component {
                         value={this.state.name}
                         onChange={this.updateFormValue}
                     />
+                </div>
+
+                <div className="form-group">
+                    <label>Ice cream flavors</label>
+                    <select className="form-control"
+                        name="flavor" 
+                        value={this.state.flavor}
+                        onChange={this.updateFormValue}
+                    >
+                        {this.flavors.map(flavor =>
+                            <option value={flavor} key={flavor}>{flavor}</option>
+                        )}
+                    </select>    
+                </div>
+
+                <div className="form-group">
+                    <label>Ice cream toppings</label>
+                    <select className="form-control"
+                        multiple={true}
+                        name="toppings" 
+                        value={this.state.toppings}
+                        onChange={this.updateFormValueOptions}
+                    >
+                        {this.toppings.map(top =>
+                            <option value={top} key={top}>{top}</option>
+                        )}
+                    </select>    
                 </div>
             </div>
         )
